ec45e342f4a07d81e5727da4f13c6672351b6b8f
Troy Chaplin <troy.chaplin@gmail.com>
2023-10-15 08:42:45 -0400

    add: input field

--- src/components/FormikContainer/FormikContainer.js
+++ src/components/FormikContainer/FormikContainer.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import { Formik, Form } from 'formik'
 import * as Yup from 'yup'
+import FormikControl from '../FormikControl/FormikControl'
 
 function FormikContainer() {
-  const initialValues = {}
-  const validationSchema = Yup.object({})
+  const initialValues = {
+    email: ''
+  }
+  const validationSchema = Yup.object({
+    email: Yup.string().required('Required')
+  })
   const onSubmit = values => console.log('Form data', values)
 
   return (
-    <Formik>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
       {
-        formike => (
-          <Form
-            initialValues={initialValues}
-            validationSchema={validationSchema}
-            onSubmit={onSubmit}
-          >
-            <submit type="submit">Submit</submit>
+        formik => (
+          <Form>
+            <FormikControl
+              control='input'
+              type="email"
+              label='email'
+              name='email'
+              placeholder="Enter a valid email"
+            />
+            <button type="submit">Submit</button>
           </Form>
         )
       }
