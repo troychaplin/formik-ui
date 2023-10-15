8f6b61b5aea590059457cb4f09488a2ba166a356
Troy Chaplin <troy.chaplin@gmail.com>
2023-10-15 11:33:57 -0400

    add: radio field

--- src/components/FormikContainer/FormikContainer.js
+++ src/components/FormikContainer/FormikContainer.js
@@ -6,21 +6,29 @@ import FormikControl from '../FormikControl/FormikControl'
 function FormikContainer() {
   const dropdownOptions = [
     { key: 'Select an Option', value: '' },
-    { key: 'Option 1', value: 'option1' },
-    { key: 'Option 2', value: 'option2' },
-    { key: 'Option 3', value: 'option3' },
+    { key: 'Option 1', value: 'selectOption1' },
+    { key: 'Option 2', value: 'selectOption2' },
+    { key: 'Option 3', value: 'selectOption3' },
+  ]
+  
+  const radioOptions = [
+    { key: 'Option 1', value: 'radioOption1' },
+    { key: 'Option 2', value: 'radioOption2' },
+    { key: 'Option 3', value: 'radioOption3' },
   ]
   
   const initialValues = {
     email: '',
     description: '',
-    selectOption: ''
+    selectOption: '',
+    radioOption: ''
   }
 
   const validationSchema = Yup.object({
     email: Yup.string().required('Required').email("Invalid email address"),
     description: Yup.string().required('Required'),
-    selectOption: Yup.string().required('Required')
+    selectOption: Yup.string().required('Required'),
+    radioOption: Yup.string().required('Required')
   })
 
   const onSubmit = values => console.log('Form data', values)
@@ -31,40 +39,44 @@ function FormikContainer() {
       validationSchema={validationSchema}
       onSubmit={onSubmit}
     >
-      {
-        formik => (
-          <Form>
-            <FormikControl
-              control='input'
-              type="name"
-              label='Name'
-              name='name'
-              placeholder="Enter your name"
-            />
-            <FormikControl
-              control='input'
-              type="email"
-              label='Email'
-              name='email'
-              placeholder="Enter a valid email"
-            />
-            <FormikControl
-              control='textarea'
-              label='Description'
-              name='description'
-              rows="5"
-              placeholder="Please describe"
-            />
-            <FormikControl
-              control='select'
-              label='Select a topic'
-              name='selectOption'
-              options={dropdownOptions}
-            />
-            <button type="submit">Submit</button>
-          </Form>
-        )
-      }
+      {formik => (
+        <Form>
+          <FormikControl
+            control='input'
+            type="name"
+            label='Name'
+            name='name'
+            placeholder="Enter your name"
+          />
+          <FormikControl
+            control='input'
+            type="email"
+            label='Email'
+            name='email'
+            placeholder="Enter a valid email"
+          />
+          <FormikControl
+            control='textarea'
+            label='Description'
+            name='description'
+            rows="5"
+            placeholder="Please describe"
+          />
+          <FormikControl
+            control='select'
+            label='Select a topic'
+            name='selectOption'
+            options={dropdownOptions}
+          />
+          <FormikControl
+            control='radio'
+            label='Radio topic'
+            name='radioOption'
+            options={radioOptions}
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      )}
     </Formik>
   )
 }
--- src/components/FormikControl/FormikControl.js
+++ src/components/FormikControl/FormikControl.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Input from '../Input/Input';
 import TextArea from '../TextArea/TextArea';
 import Select from '../Select/Select';
+import Radio from '../Radio/Radio';
 
 function FormikControl(props) {
   const { control, ...rest } = props;
@@ -11,9 +12,10 @@ function FormikControl(props) {
       return <Input {...rest} />
     case 'textarea':
       return <TextArea {...rest} />
-      case 'select':
+    case 'select':
       return <Select {...rest} />
     case 'radio':
+      return <Radio {...rest} />
     case 'checkbox':
     case 'date':
     default: return null
